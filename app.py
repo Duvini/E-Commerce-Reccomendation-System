49b2717d22380e7841cf71e85b2bb62a9e9e0f34
Duvini <it22884510@my.sliit.lk>
2024-09-11 18:53:14 +0530

    Front end (trending products)

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, render_template
+import pandas as pd
+import random
+
+
+app =Flask(__name__)
+
+# load files===========================================================================================================
+trending_products = pd.read_csv("models/trending_products.csv")
+train_data = pd.read_csv("models/clean_data.csv")
+
+# Recommendations functions============================================================================================
+# Function to truncate product name
+def truncate(text, length):
+    if len(text) > length:
+        return text[:length] + "..."
+    else:
+        return text
+#routes-------------------------------------------------------
+
+# List of predefined image URLs
+random_image_urls = [
+    "static/img/img_1.png",
+    "static/img/img_2.png",
+    "static/img/img_3.png",
+    "static/img/img_4.png",
+    "static/img/img_5.png",
+    "static/img/img_6.png",
+    "static/img/img_7.png",
+    "static/img/img_8.png",
+]
+
+@app.route('/')
+def index():
+     # Create a list of random image URLs for each product
+    random_product_image_urls = [random.choice(random_image_urls) for _ in range(len(trending_products ))]
+    price = [40, 50, 60, 70, 100, 122, 106, 50, 30, 50]
+    return render_template('index.html',trending_products=trending_products.head(8),truncate = truncate,
+                           random_product_image_urls=random_product_image_urls,
+                           random_price = random.choice(price))
+    
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
+@app.route('/index')
+def indexredirect():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
